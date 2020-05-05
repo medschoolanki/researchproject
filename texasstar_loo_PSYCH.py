a9ff23672a0a98c0cf0a99084c245fbc578c5ff8
medschoolanki <33524889+medschoolanki@users.noreply.github.com>
2020-05-05 04:49:43 -0400

    Update and rename texasstar.py to texasstar_loo_PSYCH.py

--- texasstar_loo_PSYCH.py
+++ texasstar_loo_PSYCH.py
@@ -48,7 +48,34 @@ for train_index, test_index in kf.split(X):
      confusion_matrix(y_test, model.predict(X_test))
       
       
-print(np.asarray(y_preddata))
+y_p = np.stack(y_preddata,axis=1)
+y_p = np.reshape(y_p,(622,))
+
+
+def perf_measure(y_actual, y_hat):
+    TP = 0
+    FP = 0
+    TN = 0
+    FN = 0
+    for i in range(len(y_hat)): 
+        if y_actual[i]==y_hat[i]==1:
+            TP += 1
+        if y_hat[i]==1 and y_actual[i]!=y_hat[i]:
+            FP += 1
+        if y_actual[i]==y_hat[i]==0:
+            TN += 1
+        if y_hat[i]==0 and y_actual[i]!=y_hat[i]:
+            FN += 1
+    print("TP:", TP,"FP:", FP,"TN:", TN,"FN:", FN)
+    Sensitivity = TP/(TP+FN)
+    Specificity = TN/(TN+FP)
+    PPV = TP/(TP+FP)
+    NPV = TN/(FN+TN)
+    print("Sensitivity:", Sensitivity, "Specificity:", Specificity, "PPV:", PPV, "NPV:", NPV)
+    
+
+perf_measure(y,y_p)
+
 
 #FEATURE IMPORTANCE
 features_names = ['Step1','Step2CK','AOA','CumulativeQuartile','#Ho0redClerkships','Ho0rs-AThisSpecialty','GHHS','ResearchYear','#ResearchExperiences','#Abstracts,Pres,Posters','#Peer-RevPublications','#VolunteerExperiences','#LeadershipPositions','RequiredtoRemediate','PassAttempt-Step1','PassAttempt-Step2CK','PassAttempt-Step2CS','#ProgramsApplied','#InterviewsAttended']
